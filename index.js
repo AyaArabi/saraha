59bbed55ceb7ca704e9fdba2a105c7c7b2090371
AyaArabi <aya.o.alarabi@gmail.com>
2023-12-14 03:41:24 +0300

    with vercel.json

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ import * as dotenv from 'dotenv'
 import initApp from './Src/App.router.js';
 dotenv.config();
 import connectDB from './DB/connection.js';
-import sendEmail from './Src/Services/sendEmail.js';
 
 const app =express();
 connectDB();
 initApp(app,express);
-const port =process.env.port || 3000;
+const port =process.env.port||3000;
 
 app.listen(port,()=>{
     console.log("server is running");
